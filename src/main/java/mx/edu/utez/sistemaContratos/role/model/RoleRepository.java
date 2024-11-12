8a5b5a14e70cb9915c5fa115610c911181c1bda5
alexadeher <20233tn083@utez.edu.mx>
2024-11-12 10:42:01 -0600

    Subí la carpeta de rol con los models

---
+++ src/main/java/mx/edu/utez/sistemaContratos/role/model/RoleRepository.java
@@ -0,0 +1,9 @@
+package mx.edu.utez.sistemaContratos.role.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
