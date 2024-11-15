2b2a8d12da86e8d9dd1df4a8e7108dda4945b5d4
Zohet14 <20233tn080@utez.edu.mx>
2024-11-15 08:27:26 -0600

    Subi CRUD de cliente

--- src/main/java/mx/edu/utez/sistemaContratos/cliente/control/ClienteController.java
+++ src/main/java/mx/edu/utez/sistemaContratos/cliente/control/ClienteController.java
@@ -1,4 +1,49 @@
 package mx.edu.utez.sistemaContratos.cliente.control;
 
+import mx.edu.utez.sistemaContratos.cliente.model.ClienteDto;
+import mx.edu.utez.sistemaContratos.utils.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    @Autowired
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Message> findAll() {
+        return clienteService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Message> findById(@PathVariable Long id) {
+        return clienteService.findById(id);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Message> save(@RequestBody ClienteDto clienteDto) {
+        return clienteService.save(clienteDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Message> update(@RequestBody ClienteDto clienteDto) {
+        return clienteService.update(clienteDto);
+    }
+
+    @PatchMapping("/changeStatus")
+    public ResponseEntity<Message> changeStatus(@RequestBody ClienteDto clienteDto) {
+        return clienteService.changeStatus(clienteDto);
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<Message> findByStatus(@PathVariable boolean status) {
+        return clienteService.findActives();
+    }
 }
--- src/main/java/mx/edu/utez/sistemaContratos/cliente/model/ClienteRepository.java
+++ src/main/java/mx/edu/utez/sistemaContratos/cliente/model/ClienteRepository.java
@@ -1,7 +1,13 @@
 package mx.edu.utez.sistemaContratos.cliente.model;
 
-import mx.edu.utez.sistemaContratos.usuario.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<Usuario, Long> {
+import java.util.List;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    List<Cliente> findAll();
+
+    //List<Cliente> findByStatus(boolean status);
+
+    List<Cliente> findAllByStatusIsTrue();
 }
