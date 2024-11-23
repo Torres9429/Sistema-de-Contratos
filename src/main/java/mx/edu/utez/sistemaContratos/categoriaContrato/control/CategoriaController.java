f2c57bec9183bc6c862e52520d7d7db274727b23
Torres <20233tn105@utez.edu.mx>
2024-11-23 16:37:39 -0600

    Agregué CrossOrigin para consumo en front al CategoriaController

--- src/main/java/mx/edu/utez/sistemaContratos/categoriaContrato/control/CategoriaController.java
+++ src/main/java/mx/edu/utez/sistemaContratos/categoriaContrato/control/CategoriaController.java
@@ -9,13 +9,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/categoria")
+@CrossOrigin(origins = {"*"},methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class CategoriaController {
     private final CategoriaService categoriaService;
     @Autowired
     public CategoriaController(CategoriaService categoriaService) {
         this.categoriaService = categoriaService;
     }
-    @GetMapping("/todas")
+    @GetMapping("/all")
     public ResponseEntity<Message> findAll(){
         return categoriaService.findAll();
     }
@@ -23,21 +24,21 @@ public class CategoriaController {
     public ResponseEntity<Message> findById(@PathVariable Long id) {
         return categoriaService.findById(id);
     }
-    @GetMapping("/activas")
+    @GetMapping("/actives")
     public ResponseEntity<Message> findActives(){
         return categoriaService.findActives();
     }
-    @PostMapping("/agregar")
+    @PostMapping("/save")
     public ResponseEntity<Message> save(@Validated(CategoriaDto.Register.class) @RequestBody CategoriaDto dto) {
         return categoriaService.saveCategory(dto);
     }
 
-    @PutMapping("/actualizar")
+    @PutMapping("/update")
     public ResponseEntity<Message> update(@Validated(CategoriaDto.Modify.class) @RequestBody CategoriaDto dto) {
         return categoriaService.updateCategory(dto);
     }
 
-    @PutMapping("/cambiar-status")
+    @PutMapping("/change-status")
     public ResponseEntity<Message> changeStatus(@Validated(CategoriaDto.ChangeStatus.class) @RequestBody CategoriaDto dto) {
         return categoriaService.changeStatusCategoria(dto);
     }
