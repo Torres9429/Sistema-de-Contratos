2e9644dd2c3c5bd14aea57b9d2fc6266ead5bdec
MigueDeep <20213tn055@utez.edu.mx>
2024-12-06 10:26:57 -0600

    SECURITY

--- src/main/java/mx/edu/utez/sistemaContratos/utils/DataInitializer.java
+++ src/main/java/mx/edu/utez/sistemaContratos/utils/DataInitializer.java
@@ -27,11 +27,13 @@ public class DataInitializer {
 
                 Optional<Usuario> optionalUser = usuarioRepository.findFirstByCorreo("20233tn083@utez.edu.mx");
                 if (!optionalUser.isPresent()) {
-                    Usuario usuarioAdmin = new Usuario("20233tn083@utez.edu.mx", passwordEncoder.encode("12345"));
+                    //Usuario usuarioAdmin = new Usuario("20233tn083@utez.edu.mx", passwordEncoder.encode("12345"));
+                    Usuario usuarioAdmin = new Usuario();
                     usuarioAdmin.setApellidos("Ramirez");
                     usuarioAdmin.setNombre("Juana");
                     usuarioAdmin.setTelefono("1234567890");
-
+                    usuarioAdmin.setCorreo("20233tn083@utez.edu.mx");
+                    usuarioAdmin.setContrasena(passwordEncoder.encode("12345"));
                     usuarioAdmin.getRoles().add(roleAdmin);
                     usuarioRepository.saveAndFlush(usuarioAdmin);
                 }
@@ -44,10 +46,13 @@ public class DataInitializer {
 
                 Optional<Usuario> optionalUser = usuarioRepository.findFirstByCorreo("20233tn093@utez.edu.mx");
                 if (!optionalUser.isPresent()) {
-                    Usuario userGerente = new Usuario("20233tn093@utez.edu.mx", passwordEncoder.encode("12345"));
+                   Usuario userGerente = new Usuario();
+                    // Usuario userGerente = new Usuario("20233tn093@utez.edu.mx", passwordEncoder.encode("12345"));
                     userGerente.setApellidos("Sanchez");
                     userGerente.setNombre("Ramona");
                     userGerente.setTelefono("1234567890");
+                    userGerente.setCorreo("20233tn093@utez.edu.mx");
+                    userGerente.setContrasena(passwordEncoder.encode("12345"));
                     userGerente.getRoles().add(roleGerente);
                     usuarioRepository.saveAndFlush(userGerente);
                 }
